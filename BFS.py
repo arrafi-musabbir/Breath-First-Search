cee631304921c0bae9d08cc677cbb051a3ddf6b4
arrafi-musabbir <arrafi-musabbir@github.com>
2020-10-02 08:35:33 +0600

    Added suitable comments and finalized the code to minimal

--- BFS.py
+++ BFS.py
@@ -1,8 +1,7 @@
 
 '''
- the following section opens a text file and read throw it and initializes our
- graph in a dictionary
-
+ the following section opens a text file and reads through it and initializes
+ the graph in a dictionary
 '''
 
 with open("graph(cities).txt", "r") as f:
@@ -17,41 +16,39 @@ with open("graph(cities).txt", "r") as f:
                 d[l1[0]] = l1[1:]
 
 '''
-a graph of cities have been initialized in dictionary "d"
-
+a graph of cities have been initialized in dictionary "d"'
+the following commented out code can be used to display the elements of "d"
 '''
-print(d)
+# for i in d:
+#     print(i, ":", d[i])
 
 
 def bfs(dic, Start, Goal):
-    visited = dict()
-    que = list()
-    path = list()
+    que = list()   # to be used as a stack
+    path = list()  # to keep track of the visited nodes
     temp = Start
     que.append(temp)
     while len(que) > 0:
-        temp = que.pop(0)
-        if temp not in visited.keys():
-            if dic[temp] is not None:
+        temp = que.pop(0)  # poping out the top element from stack
+
+        if path.count(temp) < 1:  # ckecking if the node has been visited
+            if dic[temp] is not None:   # checking if the node is a terminal
                 for i in dic[temp]:
-                    que.append(i)
-                    print(i)
-                visited[temp] = 1
-            else:
-                visited[temp] = 1
-            path.append(temp)
-        elif temp in visited.keys():
-            visited[temp] = visited[temp] + 1
-        if temp == Goal:
-            break
+                    que.append(i)  # pushing in to the stack
+            path.append(temp)  # enqueing the visited node to the path
+
+        if temp == Goal:  # checking if recently visited is a GOAL
+            break         # will break out if reached the GOAL node
+
     if temp == Goal:
         print("starting search-->", end=" ")
         for i in path:
             print(i, "-->", end=" ")
         print("reached GOAL")
+
     if len(que) == 0:
+        print("starting search-->", end=" ")
         print("No path exist")
 
-    #return que
 
-bfs(dic=d, Start="a", Goal="t")
+bfs(dic=d, Start="s", Goal="g")
